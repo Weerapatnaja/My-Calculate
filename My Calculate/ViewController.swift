943aeaac1a78e4abce00ebf7016b6b98d5bcf364
Student13 <weerapatit122@gmail.com>
2017-06-13 11:02:34 +0700

    Project Succes

--- My Calculate/ViewController.swift
+++ My Calculate/ViewController.swift
@@ -50,6 +50,12 @@ class ViewController: UIViewController {
         intAnswer = intNumber1 + intNumber2
         print(" intAnswer \(intAnswer) ")
         
+        //Change int to String
+        strAnswer = String(intAnswer)
+        
+        //Show String to Label
+        answerLabel.text = strAnswer
+        
     }       // answerButton
     
 
